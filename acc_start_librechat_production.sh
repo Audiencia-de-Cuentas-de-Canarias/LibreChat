f87cab01cc6d8348486682f713f58f6bbb875f21
Benito Cuesta <benito.cuesta@acuentascanarias.org>
2024-10-29 11:55:50 +0000

    build: habilitar ejecucion en produccion

---
+++ acc_start_librechat_production.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Para la correcto despliegue en el entorno de producción, se deben crear y personalizar los siguientes ficheros:
+#    - ./.env
+#    - ./client/nginx-production.conf
+#    - ./client/tls/ACC-RootCA.crt
+#    - ./client/tls/virgo.crt
+#    - ./client/tls/virgo.key
+#    - ./deploy-compose.override.yml
+#    - librechat.yml
+
+docker compose -f deploy-compose.yml -f deploy-compose.override.yml up -d
---
+++ acc_stop_librechat_production.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker compose -f deploy-compose.yml -f deploy-compose.override.yml down
