a6b75824ec9ce2e10b6cb84d249e42f81e10c608
Benito Cuesta <benito.cuesta@acuentascanarias.org>
2024-10-30 08:41:56 +0000

    chore: permitir borrar los volumenes de datos

--- acc_stop_librechat_production.sh
+++ acc_stop_librechat_production.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker compose -f deploy-compose.yml -f deploy-compose.override.yml down
+docker compose -f deploy-compose.yml -f deploy-compose.override.yml down $1
+
+if [ "$1" != "--volumes" ]
+then
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    echo -e "${RED}IMPORTANTE${NC}: Ejecutar con el parámetro --volumes para eliminar los volumenes y todos los datos asociados (reset de fabrica)"
+fi
